fd63b0a6e20718929b36af02c4360e0b8f4a0983
Lacrom <80358801+Lacrom@users.noreply.github.com>
2021-04-22 15:46:20 +0200

    Add files via upload
    -Aplikacja wersja II,
    -Nie działa,
    -Uruchamia się,
    -2 okna powstają-błąd,
    -można przejść na drugą stronę, ale brak możliwości powrotu.

---
+++ Fotowoltaika Aplikacja Druga/Fotowoltaika Aplikacja/Commands/Command_OknoWyboruSciezki.cs
@@ -0,0 +1,19 @@
+﻿using Fotowoltaika_Aplikacja.Store;
+using Fotowoltaika_Aplikacja.ViewModels;
+
+namespace Fotowoltaika_Aplikacja.Commands
+{
+    public class Command_OknoWyboruSciezki : CommandClass
+    {
+        private readonly NavigationStore _navigationStore;
+
+        public Command_OknoWyboruSciezki(NavigationStore navigationStore)
+        {
+            _navigationStore = navigationStore;
+        }
+        public override void Execute(object parameter)
+        {
+            _navigationStore.ObecnyWidokModel = new Okno_wyboru_sciezki_model();
+        }
+    }
+}
---
+++ Fotowoltaika Aplikacja Druga/Fotowoltaika Aplikacja/ViewModels/Pokaz_wyniki_model.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fotowoltaika_Aplikacja.ViewModels
+{
+    public class Pokaz_wyniki_model
+    {
+    }
+}
