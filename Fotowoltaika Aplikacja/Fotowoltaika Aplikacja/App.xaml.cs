c914808e0e1cd20fb2148cfe1446c1abf87a431c
Matyzkol98 <56502598+Matyzkol98@users.noreply.github.com>
2021-04-25 21:06:14 +0200

    Add files via upload

---
+++ Fotowoltaika Aplikacja/Fotowoltaika Aplikacja/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using Fotowoltaika_Aplikacja.ViewModels;
+using Fotowoltaika_Aplikacja.Store;
+
+namespace Fotowoltaika_Aplikacja
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            NavigationStore navigationStore = new NavigationStore();
+            navigationStore.ObecnyWidokModel = new Glowny_widok_model(navigationStore);
+
+            MainWindow mainWindow = new MainWindow()
+            {
+                DataContext = new Main_Window_View_Model(navigationStore)
+            };
+
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
